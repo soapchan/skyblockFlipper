09005288f671940b52c6b58458a7d3e658a9f5cc
soapchan <noahfoxgaming@gmail.com>
2023-11-30 11:10:45 +1100

    1

--- main.py
+++ main.py
@@ -2,80 +2,97 @@ import requests
 import tkinter as tk
 from tkinter import ttk
 
+
 # Function to fetch data from the Hypixel API
 def fetch_bazaar_data():
-    url = "https://api.hypixel.net/skyblock/bazaar"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        return data['products']
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return {}
+	url = "https://api.hypixel.net/skyblock/bazaar"
+	try:
+		response = requests.get(url)
+		response.raise_for_status()
+		data = response.json()
+		return data['products']
+	except requests.RequestException as e:
+		print(f"Error fetching data: {e}")
+		return {}
+
 
 # Function to process data and calculate profits
 def calculate_profits(data, user_money):
-    profits = []
-    for item, details in data.items():
-        # Use the highest buy order and lowest sell offer
-        buy_price = max(detail['pricePerUnit'] for detail in details['buy_summary']) if details['buy_summary'] else 0
-        sell_price = min(detail['pricePerUnit'] for detail in details['sell_summary']) if details['sell_summary'] else 0
-        profit_per_item = sell_price - buy_price
-        quantity = int(user_money / buy_price) if buy_price else 0
-        total_profit = quantity * profit_per_item
+	profits = []
+	for item, details in data.items():
+		if 'buy_summary' in details and 'sell_summary' in details and details['buy_summary'] and details[
+			'sell_summary']:
+			# Use the highest buy order and lowest sell offer
+			highest_buy_order = max(detail['pricePerUnit'] for detail in details['sell_summary'])
+			lowest_sell_offer = min(detail['pricePerUnit'] for detail in details['buy_summary'])
 
-        profits.append((item, buy_price, sell_price, profit_per_item, quantity, total_profit))
+			# Adjust prices as per original logic
+			buy_price = highest_buy_order + 0.1
+			sell_price = lowest_sell_offer - 0.1
 
-    # Sort by total profit in descending order and select top 50
-    profits.sort(key=lambda x: x[5], reverse=True)
-    return profits[:50]
+			if sell_price > buy_price:
+				profit_per_item = sell_price - buy_price
+				quantity = int(user_money / buy_price)
+				total_profit = quantity * profit_per_item
+
+				if total_profit > 0:
+					profits.append((item, buy_price.__round__(2), sell_price.__round__(2), profit_per_item.__round__(2),
+									quantity, total_profit))
+
+	# Sort by total profit in descending order and select top 50
+	profits.sort(key=lambda x: x[5], reverse=True)
+	return profits[:50]
 
 
 # Function to update the Treeview with new data
 def update_treeview(tree, data):
-    for i in tree.get_children():
-        tree.delete(i)
-    for item in data:
-        tree.insert('', 'end', values=item)
+	for i in tree.get_children():
+		tree.delete(i)
+	for item in data:
+		tree.insert('', 'end', values=item)
+
 
 # Function to refresh data
 def refresh_data(tree, money_entry):
-    try:
-        user_money = float(money_entry.get())
-    except ValueError:
-        print("Invalid money amount")
-        return
+	try:
+		user_money = float(money_entry.get())
+	except ValueError:
+		print("Invalid money amount")
+		return
 
-    data = fetch_bazaar_data()
-    profits = calculate_profits(data, user_money)
-    update_treeview(tree, profits)
+	data = fetch_bazaar_data()
+	profits = calculate_profits(data, user_money)
+	update_treeview(tree, profits)
+
 
 # Function to create the GUI
 def create_gui():
-    root = tk.Tk()
-    root.title("Skyblock Bazaar Flipping Calculator")
+	root = tk.Tk()
+	root.title("Skyblock Bazaar Flipping Calculator")
 
-    money_label = tk.Label(root, text="Enter Your Money:")
-    money_label.pack(pady=5)
+	money_label = tk.Label(root, text="Enter Your Money:")
+	money_label.pack(pady=5)
 
-    money_entry = tk.Entry(root)
-    money_entry.pack(pady=5)
+	money_entry = tk.Entry(root)
+	money_entry.pack(pady=5)
 
-    refresh_button = tk.Button(root, text="Calculate Profits", command=lambda: refresh_data(tree, money_entry))
-    refresh_button.pack(pady=10)
+	refresh_button = tk.Button(root, text="Calculate Profits", command=lambda: refresh_data(tree, money_entry))
+	refresh_button.pack(pady=10)
 
-    tree = ttk.Treeview(root, columns=("Item Name", "Buy Order at", "Sell Offer at", "Profit per Item", "Quantity", "Total Profit"), show='headings')
-    for col in tree["columns"]:
-        tree.heading(col, text=col)
+	tree = ttk.Treeview(root, columns=(
+		"Item Name", "Buy Order at", "Sell Offer at", "Profit per Item", "Quantity", "Total Profit"), show='headings')
+	for col in tree["columns"]:
+		tree.heading(col, text=col)
 
-    tree.pack(expand=True, fill='both')
+	tree.pack(expand=True, fill='both')
 
-    return root
+	return root
+
 
 def main():
-    root = create_gui()
-    root.mainloop()
+	root = create_gui()
+	root.mainloop()
+
 
 if __name__ == "__main__":
-    main()
+	main()
